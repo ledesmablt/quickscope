a5ae06012a42ed7379a28f27c75893993a06ea1b
Benj Ledesma <benj.ledesma@gmail.com>
2022-01-30 17:52:58 +0800

    Tweak SearchItem styling

--- src/NewTab/components/SearchItem/index.tsx
+++ src/NewTab/components/SearchItem/index.tsx
@@ -1,19 +1,32 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useMemo } from 'react'
 import { SearchEntry } from 'src/types'
 
 interface Props {
   searchEntry: SearchEntry
 }
 const SearchItem = ({ searchEntry }: Props): ReactElement => {
+  const infoText = useMemo(() => {
+    if (searchEntry.description) {
+      return searchEntry.description
+    }
+    try {
+      let { host } = new URL(searchEntry.url)
+      host = host.replace(/^www\./, '')
+      return host
+    } catch {
+      return searchEntry.url
+    }
+  }, [searchEntry.url, searchEntry.description])
+
   return (
     <a href={searchEntry.url}>
       <div className='inline-block w-full flex justify-between items-end'>
-        <span className='truncate grow'>
+        <span className='truncate w-[calc(100%-6rem)]'>
           {searchEntry.title || searchEntry.url}
         </span>
         {searchEntry.title && (
-          <span className='truncate text-xs pl-2'>
-            {searchEntry.description || searchEntry.url}
+          <span className='truncate text-xs text-right pl-2 pb-1 w-[6rem]'>
+            {infoText}
           </span>
         )}
       </div>

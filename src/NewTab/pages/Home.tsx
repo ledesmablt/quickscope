7394f5e3d684accbbd4b1ec3e2fa9dcd428db6b3
Benj Ledesma <benj.ledesma@gmail.com>
2022-01-30 14:18:21 +0800

    Add filter icon

--- src/NewTab/pages/Home.tsx
+++ src/NewTab/pages/Home.tsx
@@ -1,10 +1,24 @@
 import React, { ReactElement } from 'react'
 import Search from '../components/Search'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilter } from '@fortawesome/free-solid-svg-icons'
+
 const Home = (): ReactElement => {
   return (
     <div className='grow flex flex-col w-full max-h-[20rem] tall:max-h-[min(80vh,40rem)]'>
-      <p className='pb-2'>quickscope</p>
+      <div className='flex justify-between items-start pb-2'>
+        <p className='text-sm'>quickscope</p>
+        <div>
+          <button
+            onClick={() => {
+              alert('open filter modal')
+            }}
+          >
+            <FontAwesomeIcon icon={faFilter} size={'sm'} />
+          </button>
+        </div>
+      </div>
       <Search />
       <div className='pb-4 tall:pb-8' />
     </div>
